8b361f2ef383cc029ee8f91c8a1535cb18d4cfc6
root <root@srv1727706.hstgr.cloud>
2026-07-27 01:33:41 +0000

    add gunicorn, remove migrate from entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 set -e
-
-# The db service is gated by a healthcheck in compose, but keep a short retry
-# loop so migrations don't race a not-quite-ready Postgres.
-echo "Applying database migrations..."
-python manage.py migrate --noinput
-
 exec "$@"
