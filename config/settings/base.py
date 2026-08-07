abb9edfa962efdf766e8ae24d97605ffa397c01e
Imran Khan Rafa <emailofimran1992@gmail.com>
2026-08-07 16:23:06 +0600

    enable page number pagination, page size 15

--- config/settings/base.py
+++ config/settings/base.py
@@ -127,6 +127,8 @@ REST_FRAMEWORK = {
         "login": "10/min",
     },
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 15,
 }
 
 SIMPLE_JWT = {
